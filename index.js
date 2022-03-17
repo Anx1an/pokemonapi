3e2966d0975f09ef70f18f90cc4d997e5371bced
Anxian <anxian@Venerable.RedLotus>
2022-03-17 17:02:00 +0545

    test learn

---
+++ index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("id1").addEventListener("click", asd)
+})
+
+function asd() {
+    console.log("Ujjwal here");
+}
